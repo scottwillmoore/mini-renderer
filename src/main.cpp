527406fdcf15c1e806784930d9c7e2cf291bf65c
Scott Moore <scott.will.moore@gmail.com>
2020-11-12 14:59:55 +1100

    Add glfw and glm to cmake

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 
+#define GLFW_INCLUDE_NONE
+#define GLFW_INCLUDE_VULKAN
+#include <glfw/glfw3.h>
+
+#define GLM_FORCE_RADIANS
+#include <glm/glm.hpp>
+
 int main()
 {
     std::cout << "Hello, world!" << std::endl;
+
     return 0;
 }
